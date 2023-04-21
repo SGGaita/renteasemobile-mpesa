7b7cf2d4db5dd3d66b25e2a49d0a80074949fa82
SGGaita <steveggaita@gmail.com>
2023-04-21 08:18:41 +0300

    add service key

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require('firebase-admin/app')
 const { getFirestore } = require("firebase-admin/firestore")
 
-const serviceAccount = require('./u-abiri-firebase-creds.json')
+const serviceAccount = require('./rentease-115a6-firebase-creds.json')
 
 initializeApp({
     credential: cert(serviceAccount)
